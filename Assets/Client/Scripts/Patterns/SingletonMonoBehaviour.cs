5a4e8d546fbf0c724a23bba9d2b41fbf7c0f177c
hurriCase <dmitriy.pastanovskiy@gmail.com>
2024-11-19 07:49:50 +0300

    [feat] singleton pattern

---
+++ Assets/Client/Scripts/Patterns/SingletonMonoBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Client.Scripts.Patterns
+{
+    internal abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        internal static T Instance { get; private set; }
+
+        protected virtual bool IsPersistent { get; }
+
+        private void Awake()
+        {
+            if (Instance)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this as T;
+
+            if (IsPersistent)
+                DontDestroyOnLoad(this);
+
+            OnAwake();
+        }
+
+        protected virtual void OnAwake() { }
+    }
+}
