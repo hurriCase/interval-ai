9dc88e0fa9249f29c0e3def68f89c8dec70006b2
hurriCase <dmitriy.pastanovskiy@gmail.com>
2024-11-22 18:15:56 +0300

    [feat] instantiating objects through singleton pattern

---
+++ Assets/Client/Scripts/Patterns/ResourceAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Client.Scripts.Patterns
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    internal sealed class ResourceAttribute : Attribute
+    {
+        internal string Name { get; }
+        internal string Path { get; }
+
+        internal ResourceAttribute(string name, string path)
+        {
+            Name = name;
+            Path = path;
+        }
+    }
+}
---
+++ Assets/Client/Scripts/PrefabLoader.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Client.Scripts.Patterns
+{
+    internal static class PrefabLoader
+    {
+        internal static GameObject LoadDontDestroyOnLoad() => Resources.Load<GameObject>("P_DontDestroyOnLoad");
+    }
+}
