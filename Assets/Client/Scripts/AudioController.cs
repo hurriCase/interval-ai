9a53121ec498998013147964fb77fbb946a82516
hurriCase <dmitriy.pastanovskiy@gmail.com>
2024-11-22 18:23:21 +0300

    [import] vHierarchy 2 v2.0.18

---
+++ Assets/Client/Scripts/AudioController.cs
@@ -0,0 +1,28 @@
+﻿using Client.Scripts.Patterns;
+using UnityEngine;
+
+namespace Client.Scripts
+{
+    [Resource("P_AudioManager", "DontDestroyOnLoad/P_AudioManager")]
+    internal sealed class AudioController : SingletonMonoBehaviour<AudioController>
+    {
+        [SerializeField, RequiredField] private AudioSource _musicSource;
+        [SerializeField, RequiredField] private GameObject _effectsSource;
+
+        [SerializeField, RequiredField] private AudioClip[] _musicClip;
+
+        internal void PlayMusic(float volume = 1f)
+        {
+            _musicSource.volume = volume;
+            _musicSource.clip = GetRandomClip(_musicClip);
+            _musicSource.Play();
+        }
+
+        private AudioClip GetRandomClip(AudioClip[] clips)
+        {
+            return clips[Random.Range(0, clips.Length)];
+        }
+
+        protected override bool IsPersistent => true;
+    }
+}
