7b1cc6b14066fdcfe335e7f50a49b57b9cd0715e
hurriCase <dmitriy.pastanovskiy@gmail.com>
2024-11-23 17:58:09 +0300

    [feat] Main scene controller

---
+++ Assets/Client/Scripts/MainSceneController.cs
@@ -0,0 +1,15 @@
+using Client.Scripts.UI.Base;
+using Client.Scripts.UI.MainWindows;
+using UnityEngine;
+
+namespace Client.Scripts
+{
+    internal sealed class MainSceneController : MonoBehaviour
+    {
+        private void Start()
+        {
+            WindowsController.Instance.Init();
+            WindowsController.Instance.OpenWindow<MainWindow>();
+        }
+    }
+}
