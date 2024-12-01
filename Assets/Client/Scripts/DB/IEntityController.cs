e6a704855296ce04d9e6146c5deb90bded2ba31c
hurriCase <dmitriy.pastanovskiy@gmail.com>
2024-12-01 13:11:04 +0300

    [fix] missing namespace fix after changing db folder name

--- Assets/Client/Scripts/DB/IEntityController.cs
+++ Assets/Client/Scripts/DB/IEntityController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Client.Scripts.Database
+namespace Client.Scripts.DB
 {
     internal interface IEntityController
     {
