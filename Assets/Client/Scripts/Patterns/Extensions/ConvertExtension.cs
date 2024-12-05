e38a4911abe114380b02cb22e489ac39906871a8
hurriCase <dmitriy.pastanovskiy@gmail.com>
2024-12-05 17:28:27 +0300

    [feat] extensions for converting from async/await to coroutine

---
+++ Assets/Client/Scripts/Patterns/Extensions/ConvertExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+
+namespace Client.Scripts.Patterns.Extensions
+{
+    internal static class ConvertExtension
+    {
+        internal static IEnumerator WaitForTask(this Task task)
+        {
+            while (task.IsCompleted is false)
+            {
+                yield return null;
+            }
+
+            if (task.Exception != null)
+                throw task.Exception;
+        }
+    }
+}
