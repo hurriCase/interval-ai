bb4a376016d3260212c2776b129a9ea058b676cc
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-04-06 21:03:09 +0300

    [feat] ImagePixelPerUnitAdjuster

---
+++ Assets/Client/Scripts/UI/Base/ImagePixelsPerUnitAdjuster.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Client.Scripts.UI.Base
+{
+    [RequireComponent(typeof(Image))]
+    [ExecuteInEditMode]
+    internal sealed class ImagePixelsPerUnitAdjuster : MonoBehaviour
+    {
+        [SerializeField] private float _additionalPixelPerUnitMultiplier = 2f;
+
+        private Image _image;
+        private RectTransform _rectTransform;
+        private AspectRatioFitter _parentAspectRatioFitter;
+
+        private void OnEnable()
+        {
+            _image = GetComponent<Image>();
+            _rectTransform = GetComponent<RectTransform>();
+
+            if (transform.parent)
+                _parentAspectRatioFitter = transform.parent.GetComponent<AspectRatioFitter>();
+
+            UpdateImagePixelPerUnit();
+        }
+
+        private void Update()
+        {
+            if (!_parentAspectRatioFitter && transform.parent)
+                _parentAspectRatioFitter = transform.parent.GetComponent<AspectRatioFitter>();
+
+            UpdateImagePixelPerUnit();
+        }
+
+        private void UpdateImagePixelPerUnit()
+        {
+            if (!_image || !_image.sprite || !_parentAspectRatioFitter)
+                return;
+
+            var imageWidth = _image.sprite.rect.width;
+            var elementImageRatio = _rectTransform.rect.width / imageWidth;
+            var aspectRatio = _parentAspectRatioFitter.aspectRatio;
+
+            _image.pixelsPerUnitMultiplier = aspectRatio / elementImageRatio * _additionalPixelPerUnitMultiplier;
+        }
+    }
+}
