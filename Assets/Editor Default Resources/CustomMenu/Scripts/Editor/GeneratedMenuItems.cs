635c1792e23b7dfac6126bd535e507a23b8e2208
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-01 16:19:13 +0300

    [update] CustomMenuSettings.asset

--- Assets/Editor Default Resources/CustomMenu/Scripts/Editor/GeneratedMenuItems.cs
+++ Assets/Editor Default Resources/CustomMenu/Scripts/Editor/GeneratedMenuItems.cs
@@ -7,33 +7,33 @@ namespace Editor_Default_Resources.CustomMenu.Scripts.Editor
 {
     internal static class GeneratedMenuItems
     {
-        [MenuItem("--Project--/Scenes/Login", priority = 1)]
-        private static void OpenSceneLogin()
-        {
-            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo() is false)
-                return;
-
-            var scenePath = "Assets/Client/Scenes/Login.unity";
-            EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
-        }
-
-        [MenuItem("--Project--/Scenes/Main", priority = 2)]
-        private static void OpenSceneMain()
-        {
-            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo() is false)
-                return;
-
-            var scenePath = "Assets/Client/Scenes/Main.unity";
-            EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
-        }
-
-        [MenuItem("--Project--/Scenes/StartUp", priority = 3)]
+        [MenuItem("--Project--/Scenes/Startup", priority = 1)]
         private static void OpenSceneStartUp()
         {
             if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo() is false)
                 return;
 
-            var scenePath = "Assets/Client/Scenes/StartUp.unity";
+            var scenePath = "Assets/Source/Scenes/StartUp.unity";
+            EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
+        }
+
+        [MenuItem("--Project--/Scenes/Login", priority = 2)]
+        private static void OpenSceneLogin()
+        {
+            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo() is false)
+                return;
+
+            var scenePath = "Assets/Source/Scenes/Login.unity";
+            EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
+        }
+
+        [MenuItem("--Project--/Scenes/Main", priority = 3)]
+        private static void OpenSceneMain()
+        {
+            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo() is false)
+                return;
+
+            var scenePath = "Assets/Source/Scenes/Main.unity";
             EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
         }
 
@@ -47,19 +47,6 @@ namespace Editor_Default_Resources.CustomMenu.Scripts.Editor
         private static bool ValidateToggleDefaultSceneAutoLoad()
         {
             Menu.SetChecked("--Project--/Toggle Default Scene Auto Load", DefaultSceneLoader.IsDefaultSceneSet());
-            return true;
-        }
-
-        [MenuItem("--Project--/Scripting Symbols/Toggle Is RectTransform Extended Enabled", priority = 1)]
-        private static void ToggleSymbol_IS_RECTTRANSFORM_EXTENDED_ENABLED()
-        {
-            ScriptingSymbolHandler.ToggleSymbol("IS_RECTTRANSFORM_EXTENDED_ENABLED", "ScriptingSymbol_IS_RECTTRANSFORM_EXTENDED_ENABLED");
-        }
-
-        [MenuItem("--Project--/Scripting Symbols/Toggle Is RectTransform Extended Enabled", true)]
-        private static bool ValidateToggleSymbol_IS_RECTTRANSFORM_EXTENDED_ENABLED()
-        {
-            Menu.SetChecked("--Project--/Scripting Symbols/Toggle Is RectTransform Extended Enabled", ScriptingSymbolHandler.IsSymbolEnabled("ScriptingSymbol_IS_RECTTRANSFORM_EXTENDED_ENABLED", false));
             return true;
         }
     }
