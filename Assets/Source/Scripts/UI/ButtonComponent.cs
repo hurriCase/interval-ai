e54386253dda81f2a3004babfb67e061a4836db2
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-14 20:41:29 +0300

    [refactor] extracted RequiredBehaviour.cs

--- Assets/Source/Scripts/UI/ButtonComponent.cs
+++ Assets/Source/Scripts/UI/ButtonComponent.cs
@@ -1,11 +1,12 @@
-﻿using Source.Scripts.Core.Audio;
+﻿using CustomUtils.Runtime.CustomBehaviours;
+using Source.Scripts.Core.Audio;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Source.Scripts.UI
 {
     [RequireComponent(typeof(Button))]
-    internal sealed class ButtonComponent : RequiredBehaviour<Button>
+    internal sealed class ButtonComponent : ButtonBehaviour
     {
         private void Awake()
         {
