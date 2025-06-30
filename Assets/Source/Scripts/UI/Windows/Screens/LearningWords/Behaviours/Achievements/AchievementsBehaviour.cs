1ce6f9b2193e43acf3d4546b588216091c7ad11f
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-06-30 06:35:10 +0300

    [feat] WeekProgressContainer

--- Assets/Source/Scripts/UI/Windows/Screens/LearningWords/Behaviours/Achievements/AchievementsBehaviour.cs
+++ Assets/Source/Scripts/UI/Windows/Screens/LearningWords/Behaviours/Achievements/AchievementsBehaviour.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
+using CustomUtils.Runtime.Extensions;
 using R3;
 using Source.Scripts.Data.Repositories.Progress;
 using Source.Scripts.UI.Localization;
 using Source.Scripts.UI.Windows.Base;
+using Source.Scripts.UI.Windows.Shared;
 using UnityEngine;
+using ZLinq;
 
 namespace Source.Scripts.UI.Windows.Screens.LearningWords.Behaviours.Achievements
 {
@@ -11,6 +14,7 @@ namespace Source.Scripts.UI.Windows.Screens.LearningWords.Behaviours.Achievement
     {
         [SerializeField] private List<ProgressItem> _progressItems;
         [SerializeField] private ButtonComponent _achievementPopUpButton;
+        [SerializeField] private WeekProgressContainer _weekProgressContainer;
 
         internal void Init()
         {
@@ -19,19 +23,10 @@ namespace Source.Scripts.UI.Windows.Screens.LearningWords.Behaviours.Achievement
                 .AddTo(this);
 
             var currentWeek = ProgressRepository.Instance.GetCurrentWeek();
-            var localizationKeys = LocalizationKeysDatabase.Instance.WeekDaysLocalizationKeys;
-
-            if (_progressItems.Count != currentWeek.Count || localizationKeys.Count != currentWeek.Count)
-            {
-                Debug.LogError("[AchievementsBehaviour::Init] There is a mismatch between element counts:\n" +
-                               $"_progressItems.Count: {_progressItems.Count}\n" +
-                               $"currentWeek.Count: {currentWeek.Count}\n" +
-                               $"localizationKeys.Count: {localizationKeys.Count}");
-                return;
-            }
-
-            for (var i = 0; i < currentWeek.Count; i++)
-                _progressItems[i].Init(currentWeek[i], localizationKeys[i]);
+            var localizationKeys =
+                LocalizationKeysDatabase.Instance.WeekDaysLocalizationKeys.AsValueEnumerable()
+                    .Select(localizationKey => localizationKey.GetLocalization()).ToList();
+            _weekProgressContainer.UpdateWeeklyProgress(currentWeek, localizationKeys);
         }
     }
 }
---
+++ Assets/Source/Scripts/UI/Windows/Shared/WeekProgressContainer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Source.Scripts.Data.Repositories.Progress.Entries;
+using UnityEngine;
+
+namespace Source.Scripts.UI.Windows.Shared
+{
+    internal sealed class WeekProgressContainer : MonoBehaviour
+    {
+        [SerializeField] private List<ProgressItem> _progressItems;
+
+        internal void UpdateWeeklyProgress(List<DailyProgress> weeklyProgress, List<string> weekDayIdentifierTexts)
+        {
+            if (_progressItems.Count < weeklyProgress.Count || weekDayIdentifierTexts.Count < weeklyProgress.Count)
+            {
+                Debug.LogError("[AchievementsBehaviour::Init] There is a mismatch between element counts:\n" +
+                               $"_progressItems.Count: {_progressItems.Count}\n" +
+                               $"currentWeek.Count: {weeklyProgress.Count}\n" +
+                               $"localizationKeys.Count: {weekDayIdentifierTexts.Count}");
+                return;
+            }
+
+            for (var i = 0; i < _progressItems.Count; i++)
+            {
+                if ((int)weeklyProgress.First().DateTime.DayOfWeek <= i ||
+                    (int)weeklyProgress.Last().DateTime.DayOfWeek >= i)
+                    _progressItems[i].Init(weeklyProgress[i], weekDayIdentifierTexts[i]);
+                else
+                    _progressItems[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
