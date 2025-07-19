0d1091bc4e223dad5f12b1b7b958589bac13f0a1
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-07-19 17:22:52 +0300

    [feat] pluralization helper

--- Assets/Source/Scripts/UI/Localization/LocalizationKeysDatabase.cs
+++ Assets/Source/Scripts/UI/Localization/LocalizationKeysDatabase.cs
@@ -1,22 +1,32 @@
 ﻿using CustomUtils.Runtime.AssetLoader;
 using CustomUtils.Runtime.CustomTypes.Collections;
 using CustomUtils.Runtime.CustomTypes.Singletons;
-using Source.Scripts.Core;
+using CustomUtils.Runtime.Localization;
+using Source.Scripts.Data;
 using Source.Scripts.Data.Repositories.Vocabulary.Entries;
-using Source.Scripts.UI.Windows.PopUps.Achievement.Behaviours.LearningStarts.GraphProgress;
+using Source.Scripts.UI.Localization.Date;
 using UnityEngine;
+using ResourcePaths = Source.Scripts.Core.ResourcePaths;
 
 namespace Source.Scripts.UI.Localization
 {
     [Resource(ResourcePaths.ResourcePath, nameof(LocalizationKeysDatabase))]
     internal sealed class LocalizationKeysDatabase : SingletonScriptableObject<LocalizationKeysDatabase>
     {
-        [SerializeField] private EnumArray<LocalizationType, string> _localizationData;
-        [SerializeField] private EnumArray<LearningState, string> _learningStatesLocalizationData;
-        [SerializeField] private EnumArray<GraphPeriod, string> _dateLocalizationData;
+        [SerializeField] private EnumArray<LocalizationType, string> _localizationData = new(EnumMode.SkipFirst);
+        [SerializeField] private EnumArray<LearningState, string> _learningStatesLocalizationData
+            = new(EnumMode.SkipFirst);
+        [SerializeField] private EnumArray<DateType, EnumArray<PluralForm, string>> _dateLocalizationData
+            = new(EnumMode.SkipFirst);
 
         internal string GetLocalization(LocalizationType type) => _localizationData[type];
+
         internal string GetLearningStateLocalization(LearningState state) => _learningStatesLocalizationData[state];
-        internal string GetDateLocalization(GraphPeriod type) => _dateLocalizationData[type];
+
+        internal string GetDateLocalization(DateType dateType, int count)
+        {
+            var pluralForm = PluralizationHelper.GetPluralForm(count, LocalizationController.Language.Value);
+            return _dateLocalizationData[dateType][pluralForm];
+        }
     }
 }
