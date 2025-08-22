8ffa6bedc2244ead5803cf75e9334d3c55bbb864
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-08-22 07:42:50 +0300

    [project] remove unused SpacingItem.cs

--- Assets/Source/Scripts/Onboarding/UI/Screen/Behaviours/LanguageSelection/SpacingItem.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Source.Scripts.Onboarding.UI.Screen.Behaviours.LanguageSelection
-{
-    internal sealed class SpacingItem : MonoBehaviour
-    {
-        [field: SerializeField] internal CanvasGroup CanvasGroup { get; private set; }
-    }
-}
