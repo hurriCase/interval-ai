6680e448d585d0561f1069b7119da41cb27d5c3a
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-09-29 07:12:45 +0300

    [fix] parent size update with size fitter

---
+++ Assets/Source/Scripts/UI/Components/ParentSizeChangeNotifier.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Source.Scripts.UI.Components
+{
+    internal sealed class ParentSizeChangeNotifier : MonoBehaviour
+    {
+        private void OnRectTransformDimensionsChange()
+        {
+            var parent = transform.parent;
+            LayoutRebuilder.ForceRebuildLayoutImmediate(parent as RectTransform);
+        }
+    }
+}
