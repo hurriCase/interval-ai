4a3c33ee1a1831d65b2166edbc71aa56c405293a
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-10-19 14:28:05 +0300

    [feat] SystemStep with target fps

---
+++ Assets/Source/Scripts/Bootstrap/Core/Steps/SystemStep.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Source.Scripts.Bootstrap.Core.Steps
+{
+    [CreateAssetMenu(
+        fileName = nameof(SystemStep),
+        menuName = InitializationStepsPath + nameof(SystemStep)
+    )]
+    internal sealed class SystemStep : StepBase
+    {
+        protected override UniTask ExecuteInternal(CancellationToken token)
+        {
+            Application.targetFrameRate = 60;
+
+            return UniTask.CompletedTask;
+        }
+    }
+}
