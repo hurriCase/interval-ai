a20ed73d75e02f2419f790ec5fc51eadc948190b
hurriCase <dmitriy.pastanovskiy@gmail.com>
2025-10-21 15:03:15 +0300

    [refactor] CategoryCreationPopUp

--- Assets/Source/Scripts/Main/UI/PopUps/CategoryCreation/CategoryCreationPopUp.cs
+++ Assets/Source/Scripts/Main/UI/PopUps/CategoryCreation/CategoryCreationPopUp.cs
@@ -24,20 +24,24 @@ namespace Source.Scripts.Main.UI.PopUps.CategoryCreation
 {
     internal sealed class CategoryCreationPopUp : PopUpBase
     {
+        [SerializeField] private ThemeButton _createButton;
+
         [SerializeField] private TMP_InputField _categoryNameInputField;
-        [SerializeField] private ThemeButton _saveButton;
+
         [SerializeField] private RectTransform _iconsContainer;
         [SerializeField] private ToggleGroup _toggleGroup;
         [SerializeField] private StateToggle _iconItem;
+
         [SerializeField] private float _inactiveAlpha;
         [SerializeField] private string _textRequiredLocalizationKey;
         [SerializeField] private string _iconRequiredLocalizationKey;
 
-        private readonly List<StateToggle> _createdIconItems = new();
+        private readonly List<StateToggle> _iconToggles = new();
 
         private AssetReferenceSprite _selectedIcon;
-        private bool _wasFirstClick;
-        private IDisposable _disposable;
+        private bool _hasUserInteractedWithIcons;
+
+        private IDisposable _alphaSubscriptions;
 
         private INotificationComponent _notificationComponent;
         private ICategoriesRepository _categoriesRepository;
@@ -62,82 +66,101 @@ namespace Source.Scripts.Main.UI.PopUps.CategoryCreation
 
         internal override void Init()
         {
-            _saveButton.OnClickAsObservable()
-                .SubscribeUntilDestroy(this, static self => self.CreateCategory());
+            _createButton.OnClickAsObservable()
+                .SubscribeUntilDestroy(this, static self => self.TryCreateCategory());
 
-            foreach (var iconReference in _spriteReferences.CategoryIcons)
-            {
-                var createdItem = Instantiate(_iconItem, _iconsContainer);
-
-                _addressablesLoader.AssignImageAsync(createdItem.Image, iconReference, destroyCancellationToken);
-                createdItem.group = _toggleGroup;
-                createdItem.OnPointerClickAsObservable()
-                    .Do(this, iconReference, static (iconReference, self) => self._selectedIcon = iconReference)
-                    .Where(this, static self => self._wasFirstClick is false)
-                    .Do(this, static self => self._wasFirstClick = true)
-                    .SubscribeUntilDestroy(this, static self => self.SubscribeOnValueChanged());
-
-                _createdIconItems.Add(createdItem);
-            }
+            CreateIconToggles();
         }
 
         internal override UniTask ShowAsync()
         {
-            foreach (var createdIconItem in _createdIconItems)
-                createdIconItem.Image.SetAlpha(1);
+            foreach (var toggle in _iconToggles)
+                toggle.Image.SetAlpha(1f);
 
             return base.ShowAsync();
-        }
-
-        private void SubscribeOnValueChanged()
-        {
-            var builder = Disposable.CreateBuilder();
-            foreach (var createdIconItem in _createdIconItems)
-            {
-                createdIconItem.OnValueChangedAsObservable()
-                    .Subscribe((self: this, createdIconItem.Image),
-                        static (isOn, tuple) => tuple.self.OnValueChanged(isOn, tuple.Image))
-                    .AddTo(ref builder)
-                    .RegisterTo(destroyCancellationToken);
-            }
-
-            _disposable = builder.Build();
         }
 
         internal override UniTask HideAsync()
         {
             _categoryNameInputField.text = string.Empty;
             _selectedIcon = null;
-            _wasFirstClick = false;
-            _disposable.Dispose();
+            _hasUserInteractedWithIcons = false;
+            _alphaSubscriptions?.Dispose();
 
             return base.HideAsync();
         }
 
-        private void OnValueChanged(bool isOn, Graphic image)
+        private void CreateIconToggles()
         {
-            image.SetAlpha(isOn ? 1f : _inactiveAlpha);
+            foreach (var iconReference in _spriteReferences.CategoryIcons)
+            {
+                var toggle = Instantiate(_iconItem, _iconsContainer);
+                toggle.group = _toggleGroup;
+
+                _addressablesLoader.AssignImageAsync(toggle.Image, iconReference, destroyCancellationToken);
+                _iconToggles.Add(toggle);
+
+                toggle.OnPointerClickAsObservable()
+                    .SubscribeUntilDestroy(this, iconReference,
+                        static (iconReference, self) => self.HandleIconClick(iconReference));
+            }
         }
 
-        private void CreateCategory()
+        private void HandleIconClick(AssetReferenceSprite iconReference)
         {
-            if (string.IsNullOrWhiteSpace(_categoryNameInputField.text))
-            {
-                _notificationComponent.ShowMessage(_textRequiredLocalizationKey.GetLocalization());
+            _selectedIcon = iconReference;
+
+            if (_hasUserInteractedWithIcons)
                 return;
+
+            _hasUserInteractedWithIcons = true;
+            EnableAlphaChangeOnToggle();
+        }
+
+        private void EnableAlphaChangeOnToggle()
+        {
+            var builder = Disposable.CreateBuilder();
+
+            foreach (var toggle in _iconToggles)
+            {
+                var currentAlpha = toggle.isOn ? 1f : _inactiveAlpha;
+                toggle.Image.SetAlpha(currentAlpha);
+
+                toggle.OnValueChangedAsObservable()
+                    .Subscribe((toggle.Image, _inactiveAlpha),
+                        static (isOn, tuple) => tuple.Image.SetAlpha(isOn ? 1f : tuple._inactiveAlpha))
+                    .AddTo(ref builder)
+                    .RegisterTo(destroyCancellationToken);
             }
 
-            if (_selectedIcon is null)
-            {
-                _notificationComponent.ShowMessage(_iconRequiredLocalizationKey.GetLocalization());
+            _alphaSubscriptions = builder.Build();
+        }
+
+        private void TryCreateCategory()
+        {
+            if (ValidateInput() is false)
                 return;
-            }
 
             var newCategory = _categoriesRepository.CreateCategory(_categoryNameInputField.text, _selectedIcon);
             var categoryPopUp = _windowsController.OpenPopUp<CategoryPopUp>();
             categoryPopUp.SetParameters(newCategory);
 
             HideAsync();
+        }
+
+        private bool ValidateInput()
+        {
+            if (string.IsNullOrWhiteSpace(_categoryNameInputField.text))
+            {
+                _notificationComponent.ShowMessage(_textRequiredLocalizationKey.GetLocalization());
+                return false;
+            }
+
+            if (_selectedIcon is not null)
+                return true;
+
+            _notificationComponent.ShowMessage(_iconRequiredLocalizationKey.GetLocalization());
+            return false;
         }
     }
 }
